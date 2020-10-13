22311853c95d293c45c9a96a4125f1f2a278abed
Alexey Akulich <loki1987by@yandex.ru>
2020-10-13 13:19:08 +0300

    добавить схему статьи

---
+++ models/article.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+  keyword: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  source: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(str) {
+        return /https?:\/{2}\S+/gi.test(str);
+      },
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(str) {
+        return /https?:\/{2}\S+/gi.test(str);
+      },
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+});
+// **экспорт
+module.exports = mongoose.model('article', articleSchema);
