063f6164f0363ef15fba1d22425c4727b35717aa
Alexey Akulich <loki1987by@yandex.ru>
2020-10-14 21:42:27 +0300

    добавить ошибки селебрейта логину

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,15 @@
 const loginRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
+const { errorText } = require('../utils/celebrate');
 
 // **функционал
 loginRouter.post('/', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
+    email: Joi.string().required().email({ tlds: { allow: false } }).label('password')
+      .messages(errorText),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required().label('password')
+      .messages(errorText),
   }),
 }), login);
 
