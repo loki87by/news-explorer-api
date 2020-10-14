654d2c91ab95bbde35a5a2f610682623c52040ab
Alexey Akulich <loki1987by@yandex.ru>
2020-10-14 21:40:11 +0300

    улучшить ошибки целебрэйта

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -3,7 +3,7 @@ const articleRouter = require('express').Router();
 const { celebrate, Joi, CelebrateErr } = require('celebrate');
 const validator = require('validator');
 const { getAllArticles, createArticle, deleteArticle } = require('../controllers/articles');
-const { string, url } = require('../utils/celebrate');
+const { errorText } = require('../utils/celebrate');
 
 const validateUrl = (value) => {
   if (!validator.isURL(value)) {
@@ -17,15 +17,15 @@ articleRouter.get('/', getAllArticles);
 
 articleRouter.post('/', celebrate({
   body: Joi.object().keys({
-    keyword: Joi.string().required().label('keyword').messages(string),
-    title: Joi.string().required().label('title').messages(string),
-    text: Joi.string().required().label('text').messages(string),
-    date: Joi.string().required().label('date').messages(string),
-    source: Joi.string().required().label('source').messages(string),
+    keyword: Joi.string().required().label('keyword').messages(errorText),
+    title: Joi.string().required().label('title').messages(errorText),
+    text: Joi.string().required().label('text').messages(errorText),
+    date: Joi.string().required().label('date').messages(errorText),
+    source: Joi.string().required().label('source').messages(errorText),
     link: Joi.string().custom(validateUrl).required().label('link')
-      .messages(url),
+      .messages(errorText),
     image: Joi.string().custom(validateUrl).required().label('image')
-      .messages(url),
+      .messages(errorText),
   }),
 }), createArticle);
 
--- utils/celebrate.js
+++ utils/celebrate.js
@@ -1,10 +1,10 @@
-module.exports.string = {
+module.exports.errorText = {
   'string.empty': 'Поле {#label} не должно быть пустым',
   'any.required': 'Поле {#label} обязательно к заполнению',
-};
-
-module.exports.url = {
-  'string.empty': 'Поле {#label} не должно быть пустым',
-  'any.required': 'Поле {#label} обязательно к заполнению',
+  'string.min': 'В поле {#label} должно быть больше {#limit} символов',
+  'string.max': 'В поле {#label} должно быть меньше {#limit} символов',
+  'string.email': 'Поле {#label} должно содержать адрес электронной почты',
   'any.custom': 'Введите корректный URL',
+  'string.pattern.base': 'Поле {#label} не должно содержать спец.символов',
+  'string.base': 'Поле {#label} содержит спец.символы! Пароль должен состоять из больших и маленьких, а так же цифр.',
 };
