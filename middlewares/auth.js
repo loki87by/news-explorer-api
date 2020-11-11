a5e910b9867ae3b0060e8dbe2a0b3f45ee8ac3f6
Alexey Akulich <loki1987by@yandex.ru>
2020-11-11 13:53:18 +0300

    сменить место хранения ключей

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,8 +3,13 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
 const UnauthorizedError = require('../errors/unauthorized');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
